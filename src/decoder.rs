af12a51c28347a1f784f1b1dc5c9b85fe2794b7a
Cyber Phantom <inam123451@gmail.com>
2024-11-30 19:39:02 +0530

    decoder: Fix decoder

--- src/decoder.rs
+++ src/decoder.rs
@@ -21,20 +21,35 @@ impl<W: Write + Seek, R: Read> SparseDecoder<W, R> {
 
     pub fn decode(&mut self) -> std::io::Result<()> {
         let commands = self.transfer_list.commands();
+
+        let file_size = BLOCK_SIZE
+            * commands
+                .iter()
+                .flat_map(|cmd| cmd.rangeset().iter())
+                .map(|range| range.end())
+                .max()
+                .unwrap();
+
+        // Dirty hack to match file size
+        self.destination.seek(SeekFrom::Start(file_size - 1))?;
+        self.destination.write_all(&[0])?;
+
         for command in commands {
             match command {
                 Command::New(rangeset) => {
                     for range in rangeset.iter() {
-                        let start = range.start() * BLOCK_SIZE;
-                        let num_blocks = range.len();
-                        println!("Copying {num_blocks}...");
-                        self.destination.seek(SeekFrom::Start(start))?;
-                        let mut buffer = vec![0u8; (num_blocks * BLOCK_SIZE) as usize];
+                        let offset = range.start() * BLOCK_SIZE;
+                        let data_size = (range.len() * BLOCK_SIZE) as usize;
+                        println!("Writing {data_size} bytes at offset {offset}...");
+
+                        self.destination.seek(SeekFrom::Start(offset))?;
+
+                        let mut buffer = vec![0u8; data_size];
                         self.source.read_exact(&mut buffer)?;
                         self.destination.write_all(&buffer)?;
                     }
                 }
-                _ => println!("Skipping command {}", command),
+                _ => println!("Skipping command {}...", command),
             }
         }
 
